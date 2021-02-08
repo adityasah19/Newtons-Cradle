9161b0fec0aadb885d0c72157a9478e9f7d1ab21
adityasah19 <72205327+adityasah19@users.noreply.github.com>
2021-02-08 19:22:22 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,54 @@
+//Create variables here
+var dog,happydog,database,foodS,foodStock,dogimg,happydogimg
+
+function preload()
+{
+	dogimg=loadImage('images/dogimg.png')
+  happydogimg=loadImage('images/dogimg1.png')
+}
+
+
+function setup() {
+	createCanvas(500,500);
+  database=firebase.database()
+  
+  dog=createSprite(250,250,97,83)
+  dog.addImage(dogimg)
+  foodStock=database.ref('food')
+  foodStock.on('value',readStock)
+
+
+}
+
+
+function draw() {  
+background(46,139,87)
+if(keyWentDown(UP_ARROW)){
+writeStock(foodS);
+  dog.addImage(happydogimg)
+} 
+
+
+drawSprites();
+  //add styles here
+
+text('Food Remaining'+foodS,400,300)
+}
+
+function readStock(data){
+foodS=data.val();
+}
+
+function writeStock(x){
+ if(x<=0){
+   x=0
+ }else{
+   x=x-1
+ } 
+ database.ref('/').update({
+   food:x
+ }) 
+ 
+}
+
+
